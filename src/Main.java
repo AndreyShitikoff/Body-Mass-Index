5d1ddf6487dcdd21de565436a54f81198fcd80ea
Andrey Shitikov <andrey.shitikoff@gmail.com>
2020-09-23 19:32:18 +0300

    Added variables in IDEA

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
+        double weight = 87;
+        double growth = 1.68;
 
         double imb = service.calculate(87, 1.68);
 
         System.out.println(imb);
+
     }
 }
